f61960d50c3ab871075733ff3c622493598d131f
niks-narayana-dev <niks.narayana+1@gmail.com>
2024-11-16 14:05:32 +0530

    update config

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  globDirectory: "dist/",
+  globDirectory: "build/",
   globPatterns: ["**/*.{js,html,png,jpg,svg,xml,ico,json,txt,jpeg}"],
-  swDest: "dist/service-worker.js",
+  swDest: "build/service-worker.js",
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 };
