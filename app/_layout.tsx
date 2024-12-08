74c2ebde05bfde4d81658a40dfc888a007ccb6e0
niks-narayana-dev <niks.narayana+1@gmail.com>
2024-12-08 09:45:07 +0530

    move amplify configure to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -48,6 +48,16 @@ import { useFonts } from "expo-font";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { commonStyles, MainContainer } from "@/components/SharedComponents";
 import { initGA, logPageView } from "@/utils/analytics";
+import { Amplify } from "aws-amplify";
+
+Amplify.configure({
+  Analytics: {
+    Pinpoint: {
+      appId: "6b4a7558e1c044109085f007534b134d",
+      region: "us-east-1",
+    },
+  },
+});
 
 export default function Root() {
   const [loaded, error] = useFonts({
