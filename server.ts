98cbb2b8d427c4359e4ac4485f3e6e3cd8d36fcb
balamurugan-b <balaforfriends@gmail.com>
2024-12-09 17:35:33 +0000

    setting up for aws app runner

--- server.ts
+++ server.ts
@@ -36,7 +36,7 @@ app.all(
     build: SERVER_BUILD_DIR,
   }),
 );
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
